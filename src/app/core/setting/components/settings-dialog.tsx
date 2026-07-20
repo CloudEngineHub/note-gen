bd2a04192da82207c1f297c327614a1b5be7c28d
codexu <461229187@qq.com>
2026-07-20 17:21:21 +0800

    style: 优化设置弹窗宽度

--- src/app/core/setting/components/settings-dialog.tsx
+++ src/app/core/setting/components/settings-dialog.tsx
@@ -98,7 +98,7 @@ export function SettingsDialog() {
     <Dialog open={open} onOpenChange={(nextOpen) => !nextOpen && closeSettings()}>
       <DialogContent
         showCloseButton
-        className="flex h-[min(820px,calc(100vh-3rem))] w-[1280px] max-w-[calc(100vw-3rem)] gap-0 overflow-hidden p-0 sm:max-w-[calc(100vw-3rem)]"
+        className="flex h-[min(820px,calc(100vh-3rem))] w-[calc(100vw-3rem)] max-w-[1280px] gap-0 overflow-hidden p-0 sm:w-[calc(100vw-3rem)] sm:max-w-[1280px]"
       >
         <DialogTitle className="sr-only">{t('title')}</DialogTitle>
         <DialogDescription className="sr-only">{t('title')}</DialogDescription>
